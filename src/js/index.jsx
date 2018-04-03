02598832e7f3dd84204abe3dbe202975f8d6bf43
wongalofonso <alfonsowongsd@gmail.com>
2018-04-03 11:28:28 -0700

    fixed syntax

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from "react-redux";
 import App from './app';
-import rootStore from "./rootStore.js";
+import store from "./rootStore";
 
 render(
-  <Provider store = { rootStore } >
+  <Provider store = { store } >
     <App />
   </Provider>, 
   document.getElementById('root')
