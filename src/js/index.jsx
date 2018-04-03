530eb6e3847451ff0f3d90380147c81b119edb07
wongalofonso <alfonsowongsd@gmail.com>
2018-04-03 11:07:53 -0700

    fixed store

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from "react-redux";
 import App from './app';
-import rootStore from "./rootStore";
+import rootStore from "./rootStore.js";
 
 render(
-  <Provider store = {store} >
+  <Provider store = { rootStore } >
     <App />
   </Provider>, 
   document.getElementById('root')
